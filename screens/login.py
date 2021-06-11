10e9a5ce371a20517ba3b9df8acb76f556983c91
Kevin Kroos <kevinkroos@gmail.com>
2021-06-11 17:35:55 +0200

    Constant username will be set after login

--- screens/login.py
+++ screens/login.py
@@ -1,4 +1,5 @@
 from commands.user import User
+from constants import credentials
 from constants.domaintypes import DomainTypes
 from helpers.domainvalidation import DomainValidation
 from screens.main import Main
@@ -17,4 +18,5 @@ class Login:
             if result is False:
                 print('Please check your credentials')
 
+        credentials.username = username
         main = Main.index_action()
