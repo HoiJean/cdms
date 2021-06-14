60d74d62a24504a7c95b8b33cb81cc9b3d8d290f
Kevin Kroos <kevinkroos@gmail.com>
2021-06-14 20:45:02 +0200

    Added error messages

--- screens/client.py
+++ screens/client.py
@@ -12,12 +12,19 @@ class Client:
     def create():
         submitted = False
         while not submitted:
-            full_name = DomainValidation.validate(DomainTypes.full_name, 'Type the full name', '')
-            street_address = DomainValidation.validate(DomainTypes.street_address, 'Type the street address', '')
-            house_number = DomainValidation.validate(DomainTypes.house_number, 'Type the house number', '')
-            zip_code = DomainValidation.validate(DomainTypes.zip_code, 'Type the zip code', '')
-            email = DomainValidation.validate(DomainTypes.email, 'Type the email', '')
-            phone_number = DomainValidation.validate(DomainTypes.phone_number, 'Type the phone number', '')
+            full_name = DomainValidation.validate(DomainTypes.full_name, 'Type the full name', ''
+            'Fullname can only contain spaces and letters')
+            street_address = DomainValidation.validate(DomainTypes.street_address, 'Type the street address',
+            'Street cannot contain space at the begin or end and cannot contain a number')
+            house_number = DomainValidation.validate(DomainTypes.house_number, 'Type the house number', ''
+            'House number can contain numbers with optional added letter')
+            zip_code = DomainValidation.validate(DomainTypes.zip_code, 'Type the zip code',
+            'Zip code must be 4 numbers and 2 letters without a space')
+            email = DomainValidation.validate(DomainTypes.email, 'Type the email', ''
+            # Todo Add city list here
+           'Email is incorrect')
+            phone_number = DomainValidation.validate(DomainTypes.phone_number, 'Type the phone number', ''
+            'Phone number is incorrect. Note: "+31 6" is already filled in')
 
             submitted = True
 
